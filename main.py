dee6cb8139d37debc3e026d85ab2710938e29848
Nicolas Perion <github.savanna972@passmail.com>
2026-09-05 16:20:58 +0200

    completed main.py, prompt to improve for generation

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os
+
 from anki_action import AnkiClient
 from config import URL
 from download_audio import download_split_by_chapters
 from transcript import write_transcriptions
+from write_cards_content import write_cards_content
 
 deck = "Data Structures"
 client = AnkiClient()
@@ -9,3 +12,7 @@ client = AnkiClient()
 download_split_by_chapters(URL, chapters_to_exclude=["Ad"], split=True)
 write_transcriptions()
 client.create_deck(deck)
+
+for transcription_path in os.listdir("./transcriptions/"):
+    card_content = write_cards_content(transcription_path)
+    client.create_cards(deck, card_content)
--- write_cards_content.py
+++ write_cards_content.py
@@ -6,33 +6,41 @@ from config import MODEL
 SYSTEM_PROMPT = """
 You are an Anki card creator.
 You will receive information about a topic and must generate content for the fields of
-of the card (Front, Back, tags) and be compliant with the provided JSON schema.
+of the card (Front, Back) and be compliant with the provided JSON schema.
+The 'Front' field must contain one short question (at most 10 words) and the 'Back'
+field must contain the answer to that question. 
 """
 
 
 class CardFields(BaseModel):
     Front: str
     Back: str
-    tags: list[str]
+    # tags: list[str]
 
 
 schema = CardFields.model_json_schema()
 
 
-def write_cards_content(transcription: str):
+def write_cards_content(transcription_path: str) -> str:
+    """Generate the content of a card based on a transcription.
 
-    response = chat(
-        model=MODEL,
-        think="low",
-        format=schema,
-        messages=[
-            {"role": "system", "content": SYSTEM_PROMPT},
-            {
-                "role": "user",
-                "content": rf"Here is the information: <\information> {transcription}\
-                <\information>",
-            },
-        ],
-    )
+    Args:
+        transcription (str): The transcription to generate content from.
 
-    return print(response.message.content)
+    Returns:
+        str: The generated content, formated by the schema.
+    """
+    with open(transcription_path) as file:
+        response = chat(
+            model=MODEL,
+            format=schema,
+            messages=[
+                {"role": "system", "content": SYSTEM_PROMPT},
+                {
+                    "role": "user",
+                    "content": rf"Here is the information about the topic: <\information> {file.readlines()} <\information>",
+                },
+            ],
+        )
+
+    return response.message.content
